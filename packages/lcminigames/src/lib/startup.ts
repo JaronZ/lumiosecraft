2975985851957c2b35fd7e44ac0ea5b4bd9921b2
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2025-05-07 17:52:03 +0200

    Initial commit

---
+++ packages/lcminigames/src/lib/startup.ts
@@ -0,0 +1,3 @@
+global.lcminigames = {};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const lcminigames = {};
---
+++ vitest.workspace.ts
@@ -0,0 +1,6 @@
+import { defineWorkspace } from "vitest/config";
+
+export default defineWorkspace([
+	"./packages/lumiosecraft/vitest.config.ts",
+	"./packages/lcminigames/vitest.config.ts"
+]);
